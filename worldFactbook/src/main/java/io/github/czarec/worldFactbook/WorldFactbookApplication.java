c663cfb8f1afc8ab9c8a8f4db7262da96ec2d313
Czar-Ec <czar.echavez@yahoo.co.uk>
2018-11-18 20:29:18 +0000

    First commit

---
+++ worldFactbook/src/main/java/io/github/czarec/worldFactbook/WorldFactbookApplication.java
@@ -0,0 +1,12 @@
+package io.github.czarec.worldFactbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldFactbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldFactbookApplication.class, args);
+	}
+}
