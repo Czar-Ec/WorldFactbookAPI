b636e3fdfbb58c47ba84d674bafaa23d572d7557
CzarEc <czar.echavez@yahoo.co.uk>
2018-11-21 17:56:17 +0000

    factbook class that handles the scanning and ordering of the country data, currently looking at assets, need to be configurable later

---
+++ worldFactbook/src/main/java/io/github/czarec/worldFactbook/FactbookClass.java
@@ -0,0 +1,21 @@
+package io.github.czarec.worldFactbook;
+
+/**
+ * Class that holds the factbook data that the controller queries
+ * and the Scheduler hourly refreshes (rescans the file)
+ * 
+ * @author CzarEc
+ *
+ */
+public class FactbookClass {
+	
+	
+	
+	FactbookClass() {
+		
+	}
+	
+	private void scanFolder() {
+		
+	}
+}
