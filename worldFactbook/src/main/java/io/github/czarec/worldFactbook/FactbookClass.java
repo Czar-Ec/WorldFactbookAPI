77f0ad8f9ae3978f04421bd6ea5d57e807658d11
CzarEc <czar.echavez@yahoo.co.uk>
2018-11-22 23:40:19 +0000

    ???

--- worldFactbook/src/main/java/io/github/czarec/worldFactbook/FactbookClass.java
+++ worldFactbook/src/main/java/io/github/czarec/worldFactbook/FactbookClass.java
@@ -1,5 +1,7 @@
 package io.github.czarec.worldFactbook;
 
+import java.util.ArrayList;
+
 /**
  * Class that holds the factbook data that the controller queries
  * and the Scheduler hourly refreshes (rescans the file)
@@ -9,13 +11,21 @@ package io.github.czarec.worldFactbook;
  */
 public class FactbookClass {
 	
-	
+	// arraylist containing list of countries by category
+	ArrayList<ArrayList<?>> countryCategories;
 	
 	FactbookClass() {
+		countryCategories = new ArrayList<ArrayList<?>>();
 		
+		scanFolder();
 	}
 	
 	private void scanFolder() {
-		
+		// scan a folder
+		// make a key value pair, key is file name, value is the json value within
+	}
+	
+	public String getCountry() {
+		return "null for now";
 	}
 }
