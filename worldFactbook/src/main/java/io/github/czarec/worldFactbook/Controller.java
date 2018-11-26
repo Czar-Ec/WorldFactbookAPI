7beafb9abd88b6f92833348d919eb4e15401bdd2
Czar-Ec <czar.echavez@yahoo.co.uk>
2018-11-26 16:49:24 +0000

    Temporarily making my website the error page

--- worldFactbook/src/main/java/io/github/czarec/worldFactbook/Controller.java
+++ worldFactbook/src/main/java/io/github/czarec/worldFactbook/Controller.java
@@ -1,14 +1,22 @@
 package io.github.czarec.worldFactbook;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value="/factbook")
 class ControllerClass {
 
-  @RequestMapping("/status")
-  public String index() {
+  // should reroute to a new page, I should make a 404 error page on my website
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public ModelAndView index() {
+    return new ModelAndView("redirect:" + "https://czar-ec.github.io");
+  }
+
+  @RequestMapping(value = "/status")
+  public String statusCheck() {
     return "Online";
   }
 }
